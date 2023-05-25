e1d60ef8564f4249a9ea18d0a8c9dca664beff2a
JakubCzerwiec <jacob.czerwiec@gmail.com>
2023-05-25 04:59:24 +0200

    Done!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Titanic
 Machine learning tutorial #1
+
+Linear ingression
+
+Model predicts chances for survival on Titanic based on historical data like: age, gender, class of travel etc.
--- titanic.py
+++ titanic.py
@@ -18,4 +18,57 @@ dfeval = pd.read_csv('eval.csv') # testing data
 y_train = dftrain.pop('survived') # removing survived column and storing it in y_train
 y_eval = dfeval.pop('survived') # as above
 
-print(dftrain.head()) #print header of dataset
+
+# distinguish from numerical and categorical data (those who have values different then numbers)
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+# columns created from those above
+feature_columns = []
+
+# iterate over categorical columns and finds all unique values and saves it in fetures_column using ts - tensorflow
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+# same for numerical values
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+
+# Creating input function, basically it creates tensorflow DATASET OBJECT on which model trains
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+# here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+# as arguments - training dataset file and file with 'survived' column
+train_input_fn = make_input_fn(dftrain, y_train)  
+
+# only one epoch, couse it do not train, just eval, and also don't need to be shuffled
+# as arguments: as above, just testing datasets
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False) 
+
+
+# Creating a model
+# create a linear estimtaor by passing the feature columns created earlier
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns) 
+
+
+# TRAINING model
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+clear_output()  # clears console output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model, accuracy gives how good is model
+
+result = list(linear_est.predict(eval_input_fn))
+print(dfeval.loc[0]) # print person on index
+print(y_eval.loc[0]) # print if survived or not (0 or 1)
+print(result[0]['probabilities'][1]) # print what the trained model predict (probabilities on index 0 = died probability, on index 1 survival probability) for person on index 
